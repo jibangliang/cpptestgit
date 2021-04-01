1d2ddbf04222867669fa6cf9a822236b5a3aab08
bji <741440140@qq.com>
2021-04-01 15:14:01 +0900

    main.c

--- main.c
+++ main.c
@@ -4,3 +4,25 @@ int main(){
 	int b = 999;
 	return b;
 }
+int foo(int a, int b)
+{
+    int r;
+    if (a < b) {
+        r = b - a;
+    } else {
+        r = a - b;
+    }
+    return r;
+}
+
+int bar(int p, int q)
+{
+    int result;
+    if (p < q) {
+        result = q - p;
+    } else {
+        result = p - q;
+    }
+    return result;
+}
+
